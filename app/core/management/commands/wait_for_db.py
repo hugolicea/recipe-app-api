7a01c69d5a97eb7d0b87fe736132aac2ef96edd9
hugolicea <hugo.licea@gmail.com>
2025-01-31 16:06:25 -0600

    custom user model added

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,10 +7,12 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """
     Django command to wait for database to be available
     """
+
     def handle(self, *args, **options):
         """
         Django command to wait for database to be available
